0ab5c01fdf3653eeb2b379683dc06fa21684a19c
Qiwei Mao <57202568+NomiBobby@users.noreply.github.com>
2023-08-02 04:41:28 +0000

    render templates

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "How much is Messi ticket price?\n"
+    return render_template("index.html")
 
 # app.run(host="0.0.0.0", port=80)
 if __name__ == '__main__':
